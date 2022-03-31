d6e07e415a2ee34cab36bbbed9a3b845079a88dd
vertskater <vertskater@gmx.at>
2022-03-31 13:10:50 +0200

    styling and component add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import { Component } from "react";
 import Person from "./components/PersonData";
+import InputEdu from "./components/InputEdu";
 import "./style/App.scss";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      education: [],
+      showForm: false,
+    };
+  }
+  showForm = () => {
+    this.setState({ showForm: !this.state.showForm });
+  };
   render() {
     return (
       <div className="App">
         <Person />
+        <button className="btn-add" onClick={() => this.showForm()}>
+          Add Education
+        </button>
+        {this.state.showForm ? <InputEdu /> : null}
       </div>
     );
   }
---
+++ src/components/InputEdu.js
@@ -0,0 +1,7 @@
+import { Component } from "react";
+
+export default class InputEdu extends Component {
+  render() {
+    return <h1>this is input Edu</h1>;
+  }
+}
